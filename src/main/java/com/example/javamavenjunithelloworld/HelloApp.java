6c3f776ea21436b58a0d9d3a7324d7041b871aa0
t-mitani <t.mitani1988@gmail.com>
2019-01-07 01:33:40 +0900

    add checkstyle setting

--- src/main/java/com/example/javamavenjunithelloworld/HelloApp.java
+++ src/main/java/com/example/javamavenjunithelloworld/HelloApp.java
@@ -8,7 +8,7 @@ public class HelloApp {
 
     static int DEFAULT_TIMES = 3;
 
-    static int EXIT_STATUS_PARAMETER_NOT_UNDERSTOOD = 2
+    static int EXIT_STATUS_PARAMETER_NOT_UNDERSTOOD = 2;
     static int EXIT_STATUS_HELLO_FAILED = 4;
 
     /**
